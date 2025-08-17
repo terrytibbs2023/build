c9e6c892e986e0fe31b111a0e1f225e48c3e90c0
terrytibbs2023 <149832772+terrytibbs2023@users.noreply.github.com>
2025-08-17 21:55:27 +0100

    ds

--- repo21/script.terrysautoinstall/myfile (2)/addons/script.bingie.toolbox/default.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-from resources.lib.main_module import MainModule
-MainModule()
--- repo21/script.terrysautoinstall/myfile (2)/addons/script.wikipedia/resources/script.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Module: default
-# Author: jurialmunkey
-# License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
-if __name__ == '__main__':
-    import sys
-    from lib.script import Script
-    Script(*sys.argv[1:]).router()
