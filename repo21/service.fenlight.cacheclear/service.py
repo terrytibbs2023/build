de6024b2b932bcb0d6ec979958668115e0cae94b
terrytibbs2023 <149832772+terrytibbs2023@users.noreply.github.com>
2025-08-23 00:07:27 +0100

    df

--- repo21/service.fenlight.cacheclear/service.py
+++ repo21/service.fenlight.cacheclear/service.py
@@ -1,5 +1,14 @@
+import os
 import xbmc
 
-# Run silently on boot
-xbmc.executebuiltin('RunPlugin("plugin://plugin.video.fenlight/?mode=clear_cache&cache=trakt")')
+# Path to traktcache.db
+db_path = xbmc.translatePath('special://profile/addon_data/plugin.video.fenlight/databases/traktcache.db')
+
+# Delete if exists
+if os.path.exists(db_path):
+    try:
+        os.remove(db_path)
+        xbmc.log("Fenlight traktcache.db deleted on startup", xbmc.LOGINFO)
+    except Exception as e:
+        xbmc.log(f"Failed to delete traktcache.db: {e}", xbmc.LOGERROR)
 
