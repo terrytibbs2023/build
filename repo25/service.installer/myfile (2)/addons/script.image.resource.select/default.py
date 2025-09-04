11a4d7acf0d4e6da68dd7741c4f91e26765e7f8a
terrytibbs2023 <149832772+terrytibbs2023@users.noreply.github.com>
2025-09-04 22:31:24 +0100

    sss

--- repo25/service.installer/myfile (2)/addons/script.image.resource.select/default.py
+++
@@ -1,9 +0,0 @@
-import sys
-from lib import select
-
-if (__name__ == '__main__'):
-    try:
-        params = dict(arg.split('=') for arg in sys.argv[ 1 ].split('&'))
-    except:
-        params = {}
-    select.Main(params=params)
