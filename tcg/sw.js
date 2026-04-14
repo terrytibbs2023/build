31ccd72a3f373f2d0dadbcb61cb05cc779ee9de6
terrytibbs2023 <149832772+terrytibbs2023@users.noreply.github.com>
2026-04-14 18:45:35 +0100

    Update sw.js

--- tcg/sw.js
+++ tcg/sw.js
@@ -1,13 +1,20 @@
-const CACHE_NAME = 'ellis-tcg-v6';
+const CACHE_NAME = 'ellis-tcg-v7';
 const ASSETS = [
   'index.html',
   'manifest.json'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// This part is the "magic" that makes the browser trust the PWA
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
