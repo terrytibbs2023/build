4448e57a5e5b331b494bfcc16d062c121103dc47
terrytibbs2023 <149832772+terrytibbs2023@users.noreply.github.com>
2026-04-14 09:51:46 +0100

    new tcg

---
+++ tcg/sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'ellis-tcg-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
