179f8472a87cfe46bc2035419059d3e14e7489e0
terrytibbs2023 <149832772+terrytibbs2023@users.noreply.github.com>
2026-04-17 22:25:36 +0100

    kk

---
+++ ecci/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'ecc-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './ecc.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
