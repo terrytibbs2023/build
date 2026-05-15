3553f5d083c5c1e5d5ceadec124e0b05238eaffc
terrytibbs2023 <149832772+terrytibbs2023@users.noreply.github.com>
2026-05-15 20:19:20 +0100

    dddd

--- all/service.test/service.py
+++ all/service.test/service.py
@@ -5,9 +5,22 @@ import xbmcaddon
 addon = xbmcaddon.Addon()
 accepted = addon.getSettingBool("accepted")
 
+message = (
+    "Unfortunately, a lot of things have changed all at once.\n\n"
+    "Fen, which was the main engine, is gone, and RealDebrid has also started blocking streams.\n\n"
+    "If you’re not already on the new version, please ask how to install it.\n\n"
+    "If the Bingie interface feels slow on your device, go to Settings and switch to Lite mode.\n\n"
+    "From now on, we’ll be using TorBox instead of Real‑Debrid. "
+    "It’s around £2.50 per month and can be paid monthly.\n\n"
+    "Press YES to hide this message permanently."
+)
+
 if not accepted:
     dialog = xbmcgui.Dialog()
-    yes = dialog.yesno("Major Changes", "Unfortunately, a lot of things have changed all at once. Fen, which was the main engine, is gone, and RealDebrid has also started blocking streams. If you’re not already on the new version, please ask how to install it. If the Bingie interface feels slow on your device, go to Settings and switch to Lite mode. From now on, we’ll be using TorBox instead of Real‑Debrid. It’s around £2.50 per month and can be paid monthly. Press Yes to hide this message permanently.")
+    dialog.textviewer("Major Changes", message)
+
+    yes = dialog.yesno("Hide Message?", "Do you want to hide this message permanently?")
 
     if yes:
         addon.setSettingBool("accepted", True)
+
