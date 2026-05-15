81910fbe3fb7a18b6b94608c7dc001c0bf447771
terrytibbs2023 <149832772+terrytibbs2023@users.noreply.github.com>
2026-05-15 18:58:07 +0100

    ddd

---
+++ essex-tcg/cali/sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'ecc-cache-v1';
+const assets = ['./index.html', './icona.jpg', './ecc.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
