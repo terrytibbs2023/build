9100d3399a6f510579b8b2632161b95bfe276dd4
terrytibbs2023 <149832772+terrytibbs2023@users.noreply.github.com>
2026-05-15 21:18:51 +0100

    sss

--- all/service.test/service.py
+++ all/service.test/service.py
@@ -10,16 +10,15 @@ message = (
     "Fen, which was the main engine, is gone, and RealDebrid has also started blocking streams.\n\n"
     "If you’re not already on the new version, please ask how to install it.\n\n"
     "If the Bingie interface feels slow on your device, go to Settings and switch to Lite mode.\n\n"
-    "From now on, we’ll be using TorBox instead of Real‑Debrid. "
+    "From now on, we’ll be using TorBox instead of RealDebrid. "
     "It’s around £2.50 per month and can be paid monthly.\n\n"
-    "Press YES to hide this message permanently."
 )
 
 if not accepted:
     dialog = xbmcgui.Dialog()
     dialog.textviewer("Major Changes", message)
 
-    yes = dialog.yesno("Hide Message?", "Do you want to hide this message permanently?")
+    yes = dialog.yesno("Did You Read That", "Did you read that? its VERY Important!")
 
     if yes:
         addon.setSettingBool("accepted", True)
